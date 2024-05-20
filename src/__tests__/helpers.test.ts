dd747e827d3801848df0da1910c18b13e86d03c7
shirodkarpushkar <shirodkarpushkar.9@gmail.com>
2024-05-20 16:33:52 +0530

    integration tests added

---
+++ src/__tests__/helpers.test.ts
@@ -0,0 +1,29 @@
+import { describe, it, expect } from "vitest";
+import { getEnvVariable } from "../utils";
+/* Generate test for getEnvVariable */
+describe("getEnvVariable", () => {
+  it("should return all values of the environment variables correctly", () => {
+    // Arrange
+    const object = {
+      VITE_URL: "https://vitejs.dev",
+      VITE_REACT_URL: "https://reactjs.org",
+      VITE_API_URL: "https://jsonplaceholder.typicode.com",
+      VITE_FLAGS_URL: "https://www.countryflags.io",
+      VITE_GOOGLE_MAPS_API_KEY: "YOUR_GOOGLE_MAPS_API_KEY",
+    };
+
+    // Act
+    const result = Object.keys(object).map((key) => getEnvVariable(key));
+    // Assert
+    expect(result).toEqual(Object.values(object));
+  });
+
+  it("should return undefined if the environment variable is not defined", () => {
+    // Arrange
+    const key = "VITE_NOT_DEFINED";
+    // Act
+    const result = getEnvVariable(key);
+    // Assert
+    expect(result).toBeUndefined();
+  });
+});
